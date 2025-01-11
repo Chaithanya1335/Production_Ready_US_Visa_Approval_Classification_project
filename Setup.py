18096d5582a5440967b094c64f7ee4905de74007
M Gnana Chaithanya <m.gnanachaithanya12@gmail.com>
2025-01-11 09:25:18 +0530

    Data Ingestion Completed

---
+++ Setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Gnana chaithanya",
+    author_email="m.gnanachaithanya12@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
---
+++ demo.py
@@ -0,0 +1,4 @@
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.start_data_ingestion()
