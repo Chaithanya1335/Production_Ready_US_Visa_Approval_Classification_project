c3d1f9ccd5baf87ae59b038772909aa330802a2b
M Gnana Chaithanya <m.gnanachaithanya12@gmail.com>
2025-01-13 10:14:00 +0530

    CI CD Added

--- Setup.py
+++ Setup.py
@@ -1,10 +1,32 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This Function return list of requirements
+
+    """
+    requirements_lst=[]
+    try:
+        with open('requirements.txt','r') as f:
+            # Read Lines form file
+            lines = f.readlines()
+            #process the each line
+            for line in lines:
+                requirement = line.strip()
+                #ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
+                    requirements_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+    return requirements_lst
+
 
 setup(
-    name="us_visa",
-    version="0.0.0",
-    author="Gnana chaithanya",
+    name="UsVisa",
+    version="0.0.1",
+    author="Gnana Chaithanya",
     author_email="m.gnanachaithanya12@gmail.com",
     packages=find_packages(),
-    install_requires = []
+    install_requires = get_requirements()
 )
